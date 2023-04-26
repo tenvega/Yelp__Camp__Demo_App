ba595d2a2eee60c75eab050bcece4f5ce6117230
tenvega <etorra@gmail.com>
2023-04-25 21:09:59 -0400

    fixed mongo connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -26,6 +27,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) =>{
     res.render('home')
 })
+
+app.get('/makecampground', async (req, res) =>{
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'})
+    await camp.save();
+    res.send(camp)
+
+})
+
 app.listen(3000, () => {
   console.log('Serving on port 3000')
 })
