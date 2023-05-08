f03f17323743ec0a05cb7d4be7c5cffb763454cc
tenvega <etorra@gmail.com>
2023-05-07 21:26:59 -0400

    added edit, update and delete functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require ('method-override')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -23,6 +24,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 
 
@@ -39,15 +41,33 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async(req, res) =>{
+app.post('/campgrounds', async(req, res) => {
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
-app.get('/campgrounds/:id', async(req, res) =>{
+app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground})
+
+})
+
+app.put('/campgrounds/:id', async (req, res) =>{
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) =>{
+    const { id } = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
 })
 
 
